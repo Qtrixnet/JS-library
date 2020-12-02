94d4afe922d7b1edc4c328c25b1829f58264ba22
Kirill <qtrixnet@yandex.ru>
2020-12-02 20:00:11 +0600

    feat: добавить toggle js анимацию

--- README.md
+++ README.md
@@ -1,26 +1,27 @@
-# Собственная библиотека
-+ container - контейнер с максимальной шириной 1140px по центру
-+ btn - стандартная кнопка
-+ btn-(primary / success / warning / danger / dark / light) - кнопка с цветным фоном
-+ btn-outline-(primary / success / warning / danger / dark / light) - цветная кнопка с прозрачным фоном
-+ btn-block - блочная кнопка на всю ширину страницы
-+ hidden - скрытие элемента
-+ visible - отображение элемента 
-+ d-none - скрытие элемента из потока
-+ d-block - отображение элемента в виде блока
-+ d-flex - флекс элемент
-+ f-centered - центрирование флекс жлемента
-+ f-space-around - свойство space-around у флекс элемента
-+ f-space-between  - свойство space-between у флекс элемента
-+ text-center - центрирование текста
-+ block-center - центрирование блока
-+ fz-16, .fz-20, .fz-24 - размер текста 16px, 20px и 24px
-+ bold, .thin, .italic - начертание bold / thin / italic
-+ text-(primary / success / warning / danger / dark / light) - цвет текста
-+ m10, m20 - margin со всех сторон 10px и 20px
-+ mt-10, mr-10, mb-10, ml-10 - margin 10px у одной из сторон
-+ mt-20, mr-20, mb-20, ml-20 - margin 20px у одной из сторон
-+ pt-10, pr-10, pb-10, pl-10 - padding 10px у одной из сторон
-+ pt-20, pr-20, pb-20, pl-20 - padding 20px у одной из сторон
-+ w-100, w-50, w-300, w-500 - ширина 100% / 50% или 300px / 500px
-+ h-100, h-50, h-300, h-500 - высота 100% / 50% или 300px / 500px
+# Личная библиотека CSS классов и JS компонентов
+## Используемые CSS классы
++ **container** - контейнер с максимальной шириной 1140px по центру
++ **btn** - стандартная кнопка
++ **btn-(primary / success / warning / danger / dark / light)** - кнопка с цветным фоном
++ **btn-outline-(primary / success / warning / danger / dark / light)** - цветная кнопка с прозрачным фоном
++ **btn-block** - блочная кнопка на всю ширину страницы
++ **hidden** - скрытие элемента
++ **visible** - отображение элемента 
++ **d-none** - скрытие элемента из потока
++ **d-block** - отображение элемента в виде блока
++ **d-flex** - флекс элемент
++ **f-centered** - центрирование флекс жлемента
++ **f-space-around** - свойство space-around у флекс элемента
++ **f-space-between**  - свойство space-between у флекс элемента
++ **text-center** - центрирование текста
++ **block-center** - центрирование блока
++ **fz-16, .fz-20, .fz-24** - размер текста 16px, 20px и 24px
++ **bold, .thin, .italic** - начертание bold / thin / italic
++ **text-(primary / success / warning / danger / dark / light)** - цвет текста
++ **m10, m20** - margin со всех сторон 10px и 20px
++ **mt-10, mr-10, mb-10, ml-10** - margin 10px у одной из сторон
++ **mt-20, mr-20, mb-20, ml-20** - margin 20px у одной из сторон
++ **pt-10, pr-10, pb-10, pl-10** - padding 10px у одной из сторон
++ **pt-20, pr-20, pb-20, pl-20** - padding 20px у одной из сторон
++ **w-100, w-50, w-300, w-500** - ширина 100% / 50% или 300px / 500px
++ **h-100, h-50, h-300, h-500** - высота 100% / 50% или 300px / 500px
--- src/js/lib/modules/effects.js
+++ src/js/lib/modules/effects.js
@@ -1,57 +1,81 @@
 import $ from "../core";
 
-$.prototype.animateOverTime = function (dur, cb, fin) {
-  let timeStart;
+$.prototype.animateOverTime = function(dur, cb, fin) {
+    let timeStart;
 
-  function _animateOverTime(time) {
-    if (!timeStart) {
-      timeStart = time;
+    function _animateOverTime(time) {
+        if (!timeStart) {
+            timeStart = time;
+        }
+
+        let timeElapsed = time - timeStart;
+        let complection = Math.min(timeElapsed / dur, 1);
+
+        cb(complection);
+
+        if (timeElapsed < dur) {
+            requestAnimationFrame(_animateOverTime);
+        } else {
+            if (typeof fin === "function") {
+                fin();
+            }
+        }
     }
 
-    let timeElapsed = time - timeStart;
-    let complection = Math.min(timeElapsed / dur, 1);
+    return _animateOverTime;
+};
 
-    cb(complection);
+$.prototype.fadeIn = function(dur, display, fin) {
+    for (let i = 0; i < this.length; i++) {
+        this[i].style.display = display || "block";
 
-    if (timeElapsed < dur) {
-      requestAnimationFrame(_animateOverTime);
-    } else {
-      if (typeof fin === "function") {
-        fin();
-      }
+        const _fadeIn = (complection) => {
+            this[i].style.opacity = complection;
+        };
+
+        const ani = this.animateOverTime(dur, _fadeIn, fin);
+        requestAnimationFrame(ani);
     }
-  }
 
-  return _animateOverTime;
+    return this;
 };
 
-$.prototype.fadeIn = function (dur, display, fin) {
-  for (let i = 0; i < this.length; i++) {
-    this[i].style.display = display || "block";
+$.prototype.fadeOut = function(dur, fin) {
+    for (let i = 0; i < this.length; i++) {
+        const _fadeOut = (complection) => {
+            this[i].style.opacity = 1 - complection;
+            if (complection === 1) {
+                this[i].style.display = "none";
+            }
+        };
 
-    const _fadeIn = (complection) => {
-      this[i].style.opacity = complection;
-    };
+        const ani = this.animateOverTime(dur, _fadeOut, fin);
+        requestAnimationFrame(ani);
+    }
 
-    const ani = this.animateOverTime(dur, _fadeIn, fin);
-    requestAnimationFrame(ani);
-  }
-
-  return this;
+    return this;
 };
 
-$.prototype.fadeOut = function (dur, fin) {
-  for (let i = 0; i < this.length; i++) {
-    const _fadeOut = (complection) => {
-      this[i].style.opacity = 1 - complection;
-      if (complection === 1) {
-        this[i].style.display = "none";
-      }
-    };
+$.prototype.fadeToggle = function(dur, display, fin) {
+    for (let i = 0; i < this.length; i++) {
+        if (window.getComputedStyle(this[i]).display === "none") {
+            this[i].style.display = display || "block";
 
-    const ani = this.animateOverTime(dur, _fadeOut, fin);
-    requestAnimationFrame(ani);
-  }
-
-  return this;
+            const _fadeIn = (complection) => {
+                this[i].style.opacity = complection;
+            };
+            const ani = this.animateOverTime(dur, _fadeIn, fin);
+            requestAnimationFrame(ani);
+        } else {
+            const _fadeOut = (complection) => {
+                this[i].style.opacity = 1 - complection;
+                if (complection === 1) {
+                    this[i].style.display = "none";
+                }
+            };
+            const ani = this.animateOverTime(dur, _fadeOut, fin);
+            requestAnimationFrame(ani);
+        }
+    }
+    return this;
 };
