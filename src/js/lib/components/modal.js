a3791e0cee753c3b397b5526f0c969430537e1b7
Kirill <qtrixnet@yandex.ru>
2020-12-02 20:48:54 +0600

    feat: создать табы

--- src/js/lib/components/modal.js
+++ src/js/lib/components/modal.js
@@ -1,62 +1,62 @@
-import $ from '../core';
+import $ from "../core";
 
-$.prototype.modal = function(created) {
-    for (let i = 0; i < this.length; i++) {
-        const target = this[i].getAttribute('data-target');
-        $(this[i]).click((e) => {
-            e.preventDefault();
-            $(target).fadeIn(500);
-            document.body.style.overflow = 'hidden';
-        });
+$.prototype.modal = function (created) {
+  for (let i = 0; i < this.length; i++) {
+    const target = this[i].getAttribute("data-target");
+    $(this[i]).click((e) => {
+      e.preventDefault();
+      $(target).fadeIn(500);
+      document.body.style.overflow = "hidden";
+    });
 
-        const closeElements = document.querySelectorAll(`${target} [data-close]`);
-        closeElements.forEach(elem => {
-            $(elem).click(() => {
-                $(target).fadeOut(500);
-                document.body.style.overflow = '';
-                if (created) {
-                    document.querySelector(target).remove();
-                }
-            });
-        });
-    
-        $(target).click(e => {
-            if (e.target.classList.contains('modal')) {
-                $(target).fadeOut(500);
-                document.body.style.overflow = '';
-                if (created) {
-                    document.querySelector(target).remove();
-                }
-            }
-        });
-    }
+    const closeElements = document.querySelectorAll(`${target} [data-close]`);
+    closeElements.forEach((elem) => {
+      $(elem).click(() => {
+        $(target).fadeOut(500);
+        document.body.style.overflow = "";
+        if (created) {
+          document.querySelector(target).remove();
+        }
+      });
+    });
+
+    $(target).click((e) => {
+      if (e.target.classList.contains("modal")) {
+        $(target).fadeOut(500);
+        document.body.style.overflow = "";
+        if (created) {
+          document.querySelector(target).remove();
+        }
+      }
+    });
+  }
 };
 
 $('[data-toggle="modal"]').modal();
 
-$.prototype.createModal = function({text, btns} = {}) {
-    for (let i = 0; i< this.length; i++) {
-        let modal = document.createElement('div');
-        modal.classList.add('modal');
-        modal.setAttribute('id', this[i].getAttribute('data-target').slice(1));
+$.prototype.createModal = function ({ text, btns } = {}) {
+  for (let i = 0; i < this.length; i++) {
+    let modal = document.createElement("div");
+    modal.classList.add("modal");
+    modal.setAttribute("id", this[i].getAttribute("data-target").slice(1));
 
-        // btns = {count: num, settings: [[text, classNames=[], close, cb]]}
-        const buttons = [];
-        for (let j = 0;j < btns.count; j++){
-            let btn = document.createElement('button');
-            btn.classList.add('btn', ...btns.settings[j][1]);
-            btn.textContent = btns.settings[j][0];
-            if (btns.settings[j][2]) {
-                btn.setAttribute('data-close', 'true');
-            }
-            if (btns.settings[j][3] && typeof(btns.settings[j][3]) === 'function') {
-                btn.addEventListener('click', btns.settings[j][3]);
-            }
+    // btns = {count: num, settings: [[text, classNames=[], close, cb]]}
+    const buttons = [];
+    for (let j = 0; j < btns.count; j++) {
+      let btn = document.createElement("button");
+      btn.classList.add("btn", ...btns.settings[j][1]);
+      btn.textContent = btns.settings[j][0];
+      if (btns.settings[j][2]) {
+        btn.setAttribute("data-close", "true");
+      }
+      if (btns.settings[j][3] && typeof btns.settings[j][3] === "function") {
+        btn.addEventListener("click", btns.settings[j][3]);
+      }
 
-            buttons.push(btn);
-        }
+      buttons.push(btn);
+    }
 
-        modal.innerHTML = `
+    modal.innerHTML = `
         <div class="modal-dialog">
             <div class="modal-content">
                 <button class="close" data-close>
@@ -77,9 +77,9 @@ $.prototype.createModal = function({text, btns} = {}) {
         </div>
         `;
 
-        modal.querySelector(".modal-footer").append(...buttons);
-        document.body.appendChild(modal);
-        $(this[i]).modal(true);
-        $(this[i].getAttribute('data-target')).fadeIn(500);
-    }
+    modal.querySelector(".modal-footer").append(...buttons);
+    document.body.appendChild(modal);
+    $(this[i]).modal(true);
+    $(this[i].getAttribute("data-target")).fadeIn(500);
+  }
 };
